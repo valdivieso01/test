a5b0a63e2868d5404d3ec6eb7fcac27a7bbe36e5
Javier Valdivieso <javier.valdivieso@possumus.tech>
2019-07-23 00:42:00 -0300

    first commit

---
+++ test.py
@@ -0,0 +1,12 @@
+import unittest
+import app
+
+class MyTest(unittest.TestCase):
+    def test_my_function(self):
+        self.assertEqual(app.my_function(1,1),2)
+        self.assertEqual(app.my_function(1, -1),0)
+        self.assertEqual(app.my_function(-1, -1),-2)
+        self.assertEqual(app.my_function(1.1, 1),2.1)
+
+if __name__ == '__main__':
+    unittest.main()
